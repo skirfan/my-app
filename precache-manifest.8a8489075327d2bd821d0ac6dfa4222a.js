a9c9df130b520856a724a29aae22ee602be515dd
Raj Shah <rsh@narola.email>
2019-11-01 14:41:10 +0530

    Updates

---
+++ precache-manifest.8a8489075327d2bd821d0ac6dfa4222a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8adc768dafb78eb6df86c6562e4e5d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5617a90a2b617ad0655a",
+    "url": "/my-app/static/js/2.1a350770.chunk.js"
+  },
+  {
+    "revision": "2998b2f1c2f6035e82a7",
+    "url": "/my-app/static/js/main.79c50351.chunk.js"
+  },
+  {
+    "revision": "4883f767f8b6e335f294",
+    "url": "/my-app/static/js/runtime-main.74992542.js"
+  }
+]);
